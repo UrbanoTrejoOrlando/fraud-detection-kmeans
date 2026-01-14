bf4a266e3d809ee6e5922747169f69dff81eff72
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2026-01-14 09:51:08 -0600

    Add kmeans API endpoint to URLs

--- kmeans_app/urls.py
+++ kmeans_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('kmeans-analysis/', views.kmeans_analysis, name='kmeans_analysis'),
     path('generate-data/', views.generate_simulated_data, name='generate_data'),
     path('api/modelo2/dataset-info/', views.dataset_info_api, name='dataset_info_api'),
+    path('api/modelo2/kmeans/', views.kmeans_api, name='modelo2_kmeans_api'),
 ]
